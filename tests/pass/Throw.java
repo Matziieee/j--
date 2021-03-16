437eea861cf3df8704d7e27dddf219095744cb26
Panagiotis8894 <73605142+Panagiotis8894@users.noreply.github.com>
2021-03-16 19:53:12 +0100

    Add files via upload

---
+++ tests/pass/Throw.java
@@ -0,0 +1,11 @@
+package pass;
+
+import java.lang.System;
+
+public class Throw{
+    public static void main(String[] args){
+        if(1>2){
+            throw IOException("Wrong!")
+        }
+    }
+}
