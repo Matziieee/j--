8600b50b870d7909c46c4ac0a9b3efc4e0ae3bf1
Panagiotis8894 <73605142+Panagiotis8894@users.noreply.github.com>
2021-03-18 15:03:14 +0100

    Add files via upload

---
+++ tests/pass/Doubles.java
@@ -0,0 +1,11 @@
+package pass;
+
+import java.lang.System;
+
+public class Doubles{
+    public double opDouble(double x, double y){
+        double x=2.3;
+        double x=0.2;
+        return x+y;
+    }
+}
